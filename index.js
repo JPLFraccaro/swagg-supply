ef9bb148209ce5089849e6ac66e015baa8d1cdde
JPLFraccaro <jplfraccaro@gmail.com>
2023-03-08 19:17:24 -0300

    express init, hello world

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hola mundo')
+const express = require('express')
+const app = express()
+const port = 5432
+
+app.get('/', (req,res) => {
+  res.send('hello world')
+})
+
+
+app.listen(port, () => {
+  console.log('la app esta ejecutandose')
+})
